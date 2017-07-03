2ef6b58ff61001bbd70683b9a2f212074ee4958d
escakot <ec@neorennie.com>
2017-07-03 14:12:39 -0400

    Initial Commit

---
+++ Two Player Math/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Two Player Math
+//
+//  Created by Errol Cheong on 2017-07-03.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Two Player Math/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Two Player Math
+//
+//  Created by Errol Cheong on 2017-07-03.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
